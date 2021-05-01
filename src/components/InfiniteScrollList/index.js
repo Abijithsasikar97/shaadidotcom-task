0d455115c2d71d5b63fcc4ae34d4658190e9bf11
Your Name <abijithsasikar22@gmail.com>
2021-05-01 08:30:48 +0530

    Added userLIst api call

--- src/components/InfiniteScrollList/index.js
+++ src/components/InfiniteScrollList/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { List, Avatar, Spin } from "antd";
 
-
 export const InfiniteScrollList = () => {
   const [userList, setUserList] = useState([]);
   const [paginationLimit, setPaginationLimit] = useState(10);
@@ -13,18 +12,23 @@ export const InfiniteScrollList = () => {
   }, []);
 
   const getuserData = () => {
-    axios
-      .get(
-        `https://jsonplaceholder.typicode.com/photos?_start=0&_limit=${paginationLimit}`
-      )
+    setShowLoader(true);
+    axios({
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        "app-id": "608cc1c54135ad3adae8b4fb",
+      },
+      url: `https://dummyapi.io/data/api/user?limit=${paginationLimit}`,
+    })
       .then((response) => {
         setShowLoader(false);
         if (paginationLimit > 10) {
-          let arry = [...userList, ...response.data];
+          let arry = [...userList, ...response.data.data];
 
           setUserList(arry);
         } else {
-          setUserList(response.data);
+          setUserList(response.data.data);
         }
       })
       .catch((error) => {
@@ -37,7 +41,6 @@ export const InfiniteScrollList = () => {
       e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight < 50;
     if (bottom) {
       let pagination = paginationLimit + 10;
-      setShowLoader(true);
       setPaginationLimit(pagination);
       setTimeout(() => {
         getuserData();
@@ -53,13 +56,13 @@ export const InfiniteScrollList = () => {
         <List
           itemLayout="horizontal"
           dataSource={userList}
-          //   loading={showLoader}
+          style={{ textAlign: "left" }}
           renderItem={(users) => (
             <List.Item>
               <List.Item.Meta
-                avatar={<Avatar src={users.thumbnailUrl} />}
-                title={users.title}
-                description={users.url}
+                avatar={<Avatar src={users.picture} />}
+                title={`${users.firstName}  ${users.lastName}`}
+                description={users.email}
               />
             </List.Item>
           )}
